991ae64ef0519045c65a24cb4b621b40ed3af7cd
Yash Ghadi <yashghadi2005@gmail.com>
2026-08-01 13:44:58 +0530

    feat: changed the database cloud

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,11 +3,10 @@ require("dotenv").config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 11894,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: { minVersion: "TLSv1.2" },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
